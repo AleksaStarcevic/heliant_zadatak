f1cb68f84aea8741e79548f34fa0837266943d76
AleksaStarcevic <aleksastarcevic1@gmail.com>
2024-02-06 21:13:37 +0100

    project initialization

---
+++ src/main/java/com/example/heliant_spring/HeliantSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.heliant_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeliantSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeliantSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/heliant_spring/HeliantSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heliant_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeliantSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
