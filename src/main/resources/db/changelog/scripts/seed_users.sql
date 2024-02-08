35a33ebba56708829257aeecc2c968c0da6e54ef
AleksaStarcevic <aleksastarcevic1@gmail.com>
2024-02-08 12:00:00 +0100

    add migration to seed users

---
+++ src/main/resources/db/changelog/scripts/seed_users.sql
@@ -0,0 +1,6 @@
+-- liquibase formatted sql
+
+-- changeset aleksa:7
+INSERT INTO user (username, password) VALUES ('aleksa@gmail.com', '$2a$12$Ea2Hy4CIYRqHBJidnu4bjO5gc29MCi5yIH4uOT.LvuK7ctUCrJ2Ky'),
+                                              ('milos@gmail.com', '$2a$12$Ea2Hy4CIYRqHBJidnu4bjO5gc29MCi5yIH4uOT.LvuK7ctUCrJ2Ky'),
+                                              ('nikola@gmail.com', '$2a$12$Ea2Hy4CIYRqHBJidnu4bjO5gc29MCi5yIH4uOT.LvuK7ctUCrJ2Ky');
